12d30e2cd85aa7b3e8a974c328d69cf83a74565c
Y Naga Pragnesh <yedrunagapragnesh@gmail.com>
2026-01-05 08:55:34 +0530

    Fix input prompts in age_calculator.py

--- age_calculator.py
+++ age_calculator.py
@@ -5,7 +5,7 @@ def age_calculator(birth_year,birth_month,birth_date):
   if (today.month,today.day) < (birth_month,birth_year):
     age -= 1
   return age
-birth_year = int(input("enter the year")
-birth_month = int(input("enter month")
-birth_date = int(input("emter date")
+birth_year = int(input("enter the year"))
+birth_month = int(input("enter month"))
+birth_date = int(input("emter date"))
 print(age_calculator(birth_year,birth_month,birth_date))
